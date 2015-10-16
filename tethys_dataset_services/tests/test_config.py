f26031a35f54c73c801c378f559329e56304ab69
Scott <sdc50@byu.net>
2015-10-16 16:00:44 -0600

    Fixed url parameter to be compatible with CKAN 2.4.4. Also, added download_resource and download_dataset methods.

--- tethys_dataset_services/tests/test_config.py
+++ tethys_dataset_services/tests/test_config.py
@@ -2,8 +2,8 @@
 
 TEST_CKAN_DATASET_SERVICE = {
     'ENGINE': 'tethys_dataset_services.engines.CkanDatasetEngine',
-    'ENDPOINT': 'http://ciwckan.chpc.utah.edu/api/3/action',
-    'APIKEY': '003654e6-cd89-46a6-9035-28e4037b44d6',
+    'ENDPOINT': '',
+    'APIKEY': '',
     'USERNAME': '',
     'PASSWORD': ''
 }
@@ -18,7 +18,7 @@ TEST_HYDRO_SHARE_DATASET_SERVICE = {
 
 TEST_GEOSERVER_DATASET_SERVICE = {
     'ENGINE': 'tethys_dataset_services.engines.GeoServerSpatialDatasetEngine',
-    'ENDPOINT': 'http://192.168.59.103:8181/geoserver/rest',
-    'USERNAME': 'admin',
-    'PASSWORD': 'geoserver'
+    'ENDPOINT': '',
+    'USERNAME': '',
+    'PASSWORD': ''
 }
