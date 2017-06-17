97d817fc8db18d681ea6aa852f8900302594a473
swainn <nathan.swain86@gmail.com>
2017-06-16 20:42:13 -0500

    Advanced patch version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = ['django',
             'requests',
             'requests_toolbelt']
 
-version = '1.6.0'
+version = '1.6.1'
 setup(
     name='tethys_dataset_services',
     version=version,
