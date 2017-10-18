3de94b152632acc78478030c6ec15ffce39b61d9
nswain <nswain@aquaveo.com>
2017-10-18 16:37:56 -0600

    Patch 1.6.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = ['django',
             'requests',
             'requests_toolbelt']
 
-version = '1.6.3'
+version = '1.6.4'
 setup(
     name='tethys_dataset_services',
     version=version,
