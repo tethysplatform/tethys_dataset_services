9c21d58ab6444e3ffb3b538358f29a72ee0378f7
teva <tveluppillai@aquaveo.com>
2018-06-28 09:28:01 -0600

    fixed bad merge

--- tethys_dataset_services/tests/geoserver_engine_tests.py
+++ tethys_dataset_services/tests/geoserver_engine_tests.py
@@ -1909,12 +1909,16 @@ class TestGeoServerDatasetEngine(unittest.TestCase):
                           self.engine.validate
                           )
 
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.requests.get')
+    def test_validate_401(self, mock_get):
         # 401 Code
         mock_get.return_value = MockResponse(401)
         self.assertRaises(AssertionError,
                           self.engine.validate
                           )
 
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.requests.get')
+    def test_validate_not_200(self, mock_get):
         # !201 Code
         mock_get.return_value = MockResponse(201)
 
@@ -1922,6 +1926,8 @@ class TestGeoServerDatasetEngine(unittest.TestCase):
                           self.engine.validate
                           )
 
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.requests.get')
+    def test_validate_not_geoserver(self, mock_get):
         # text
         mock_get.return_value = MockResponse(200, text="Bad text")
         self.assertRaises(AssertionError,
@@ -3487,5 +3493,112 @@ class TestGeoServerDatasetEngine(unittest.TestCase):
 
         mc.get_store.assert_called_with(name=self.store_names[0], workspace=self.workspace_names[0])
 
-    def test_add_table_to_postgis_store(self):
-        pass
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.requests.post')
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.GeoServerCatalog')
+    def test_add_table_to_postgis_store(self, mock_catalog, mock_post):
+        mc = mock_catalog()
+
+        mc.get_store.return_value = self.mock_stores[0]
+        mc.get_default_workspace.return_value = self.mock_workspaces[0]
+
+        store_id = self.store_names[0]
+
+        mock_post.return_value = MockResponse(201)
+
+        table_name = 'points'
+
+        response = self.engine.add_table_to_postgis_store(store_id=store_id,
+                                                          table=table_name,
+                                                          debug=False)
+
+        expected_url = '{endpoint}workspaces/{w}/datastores/{s}/featuretypes'.format(
+            endpoint=self.endpoint,
+            w=self.workspace_names[0],
+            s=self.store_names[0]
+        )
+        expected_headers = {
+            "Content-type": "text/xml",
+            "Accept": "application/xml"
+        }
+
+        # Validate response object
+        self.assert_valid_response_object(response)
+
+        self.assertTrue(response['success'])
+
+        # Extract Result
+        r = response['result']
+
+        # Type
+        self.assertIsInstance(r, dict)
+
+        self.assertIn('name', r)
+        self.assertIn(self.store_names[0], r['name'])
+
+        post_call_args = mock_post.call_args_list
+        self.assertEqual(expected_url, post_call_args[0][1]['url'])
+        self.assertEqual(expected_headers, post_call_args[0][1]['headers'])
+
+        mc.get_store.assert_called_with(name=self.store_names[0], workspace=self.workspace_names[0])
+
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.GeoServerCatalog')
+    def test_add_table_to_postgis_store_fail_request(self, mock_catalog):
+        mc = mock_catalog()
+        mc.get_store.side_effect = geoserver.catalog.FailedRequestError()
+        store_id = '{}:{}'.format(self.workspace_names[0], self.store_names[0])
+
+        table_name = 'points'
+
+        response = self.engine.add_table_to_postgis_store(store_id=store_id,
+                                                          table=table_name,
+                                                          debug=False)
+
+        # Validate response object
+        self.assert_valid_response_object(response)
+
+        self.assertFalse(response['success'])
+
+        # Extract Result
+        r = response['error']
+
+        self.assertIn('There is no store named', r)
+
+        mc.get_store.assert_called_with(name=self.store_names[0], workspace=self.workspace_names[0])
+
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.requests.post')
+    @mock.patch('tethys_dataset_services.engines.geoserver_engine.GeoServerCatalog')
+    def test_add_table_to_postgis_store_not_201(self, mock_catalog, mock_post):
+        mc = mock_catalog()
+
+        mc.get_store.return_value = self.mock_stores[0]
+
+        store_id = '{}:{}'.format(self.workspace_names[0], self.store_names[0])
+
+        mock_post.return_value = MockResponse(500)
+
+        table_name = 'points'
+
+        response = self.engine.add_table_to_postgis_store(store_id=store_id,
+                                                          table=table_name,
+                                                          debug=False)
+
+        expected_url = '{endpoint}workspaces/{w}/datastores/{s}/featuretypes'.format(
+            endpoint=self.endpoint,
+            w=self.workspace_names[0],
+            s=self.store_names[0]
+        )
+        expected_headers = {
+            "Content-type": "text/xml",
+            "Accept": "application/xml"
+        }
+
+        # Validate response object
+        self.assert_valid_response_object(response)
+
+        self.assertFalse(response['success'])
+
+        post_call_args = mock_post.call_args_list
+        self.assertEqual(expected_url, post_call_args[0][1]['url'])
+        self.assertEqual(expected_headers, post_call_args[0][1]['headers'])
+
+        mc.get_store.assert_called_with(name=self.store_names[0], workspace=self.workspace_names[0])
