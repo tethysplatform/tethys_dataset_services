85a063ca3fad759d443e419c74ba39e600001f89
Nathan Swain <nswain@aquaveo.com>
2022-01-10 14:40:57 -0700

    Why are you failing stickler?

--- tethys_dataset_services/engines/geoserver_engine.py
+++ tethys_dataset_services/engines/geoserver_engine.py
@@ -1907,7 +1907,7 @@ class GeoServerSpatialDatasetEngine(SpatialDatasetEngine):
                 self.delete_style(style_id, purge=True)
             except Exception as e:
                 if 'referenced by existing' in str(e):
-                    msg = f"Unable to overwrite style due to following error: {str(e)}"
+                    msg = "Unable to overwrite style due to following error: {0}".format(str(e))
                     log.error(msg)
                     response_dict = {
                         'success': False,
